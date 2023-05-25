8070b1b627cc5c59343f5c527fac151bb498f08d
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-05-25 14:47:19 -0300

    feat: implemented onUse listener

--- src/main/java/com/example/rainbowsnake/Main.java
+++ src/main/java/com/example/rainbowsnake/Main.java
@@ -1,6 +1,7 @@
 package com.example.rainbowsnake;
 
 import com.example.rainbowsnake.commands.CommandSnake;
+import com.example.rainbowsnake.events.SnakeEventListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -10,6 +11,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         Objects.requireNonNull(this.getCommand("snake")).setExecutor(new CommandSnake());
+        getServer().getPluginManager().registerEvents(new SnakeEventListener(), this);
     }
 
     @Override
