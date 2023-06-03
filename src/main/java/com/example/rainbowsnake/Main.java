1e0da5f16dce3d4813beb2a90263337d033421f0
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-06-03 00:45:59 -0300

    WIP: Spawn Snake tail

--- src/main/java/com/example/rainbowsnake/Main.java
+++ src/main/java/com/example/rainbowsnake/Main.java
@@ -1,7 +1,7 @@
 package com.example.rainbowsnake;
 
 import com.example.rainbowsnake.commands.CommandSnake;
-import com.example.rainbowsnake.events.SnakeEventListener;
+import com.example.rainbowsnake.events.UseSnake;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -11,11 +11,12 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         Objects.requireNonNull(this.getCommand("snake")).setExecutor(new CommandSnake());
-        getServer().getPluginManager().registerEvents(new SnakeEventListener(), this);
+        getServer().getPluginManager().registerEvents(new UseSnake(this), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
---
+++ src/main/java/com/example/rainbowsnake/events/UseSnake.java
@@ -0,0 +1,61 @@
+package com.example.rainbowsnake.events;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.Objects;
+
+public class UseSnake implements Listener {
+
+    private final Plugin plugin;
+
+    public UseSnake(Plugin plugin){
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onUseSnake(PlayerInteractEvent event) {
+
+        Action action = event.getAction();
+        if(action.equals(Action.RIGHT_CLICK_AIR)) {
+
+            ItemStack itemInHand = event.getItem();
+
+            assert itemInHand != null;
+            if (itemInHand.getType().equals(Material.FEATHER)) {
+
+                if (Objects.requireNonNull(itemInHand.getItemMeta()).getDisplayName().equalsIgnoreCase("Rainbow Snake")) {
+
+                    Player player = event.getPlayer();
+                    player.sendMessage("Used Rainbow Snake!!");
+                    new BukkitRunnable() {
+                        int tickCount = 0;
+                        @Override
+                        public void run() {
+
+                            World world = player.getWorld();
+                            FallingBlock fb = world.spawnFallingBlock(new Location(world, 0, 80, 0), Material.BLUE_STAINED_GLASS.createBlockData());
+                            fb.setVelocity(new Vector(1, 1, 0));
+                            fb.setDropItem(true);
+
+                            tickCount++;
+                            if(tickCount == 20) this.cancel();
+                        }
+                    }.runTaskTimer(plugin, 0,5);
+                }
+            }
+
+        }
+    }
+}
